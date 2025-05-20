282be6de3862b371cd74458d5c6b81f6609d995f
Netesh5 <netesh.paudel@gmail.com>
2025-05-20 15:19:34 +0545

    chore: changes in config files

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
-import (
-	"log"
-	"os"
-)
+import "github.com/netesh5/go_ecommerce/internal/config"
 
-func init() {
-
-	if _, err := os.Stat(".env"); err != nil {
-		log.Println("Error loading .env file")
-	}
-	log.Println("Loading .env file")
+func main() {
+	config := config.LoadConfig()
+	println("Config loaded successfully", config.Env)
+	// if _, err := os.Stat(".env"); err != nil {
+	// 	log.Println("Error loading .env file")
+	// }
+	// log.Println("Loading .env file")
 
 }
