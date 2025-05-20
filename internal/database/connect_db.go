fb6b0839c07509a68fdcda222b1a7e2304eb8047
Netesh5 <netesh.paudel@gmail.com>
2025-05-20 23:03:55 +0545

    add db connection function

--- internal/database/connect_db.go
+++ internal/database/connect_db.go
@@ -1,8 +1,23 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"log"
 
-func ConnectDB() {
-	postgresInfo =
-		sql.Open("postgres")
+	"github.com/netesh5/go_ecommerce/internal/config"
+)
+
+func ConnectDB(cfg *config.Config) (*sql.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DbConfig.Host,
+		cfg.DbConfig.Port,
+		cfg.DbConfig.User,
+		cfg.DbConfig.Password,
+		cfg.DbConfig.DbName)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
+	}
+	return db, nil
 }
